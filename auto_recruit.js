35fdbfb6e91da15b762107bb0ff83c2dc9b26b7d
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2018-08-29 23:54:00 +0900

    複数応募に対応

--- auto_recruit.js
+++ auto_recruit.js
@@ -1,37 +1,24 @@
-const pptr = require('puppeteer');
-const url = require('url');
-const request = require('sync-request');
-require('dotenv').config();
+require('dotenv').config()
+const urlUtil = require('./utils/url')
+const apiUtil = require('./utils/api')
+const puppeteer = require('./utils/pptr')
 
-async function run(){
-    const cwInfoAPI = 'https://anatamo-kirei.jp/wp-json/wp/v2/cwinfo'
-    let response = request('GET', cwInfoAPI)
-    const target = JSON.parse(response.body)
-    console.log("[INFO] Target Offer: " + target.offer_url)
-    let u = new url.Url()
-    u.protocol = 'https'
-    u.host = 'crowdworks.jp'
-    const params = process.env.CI ? {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    } : {
-        headless: false,
-        slowMo: 50
-    };
-    const browser = await pptr.launch(params);
-    const page = await browser.newPage()
-    await page.goto('https://crowdworks.jp/login?ref=root_pages-index-header')
+const recruit = async (browser, url) => {
+    console.log("[INFO] Target Job: " + url)
+    let page = await browser.newPage()
+    await page.goto(urlUtil.cw)
     await page.type('input[name="username"]', process.env.CW_USERNAME)
     await page.type('input[name="password"]', process.env.CW_PASSWORD)
     const loginBtn = await page.$('input[name="commit"]')
     loginBtn.click()
-    await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"})
+    await page.waitForNavigation({timeout: 90000, waitUntil: "domcontentloaded"})
     console.log("[INFO] Logined")
-    await page.goto(target.offer_url)
+    await page.goto(url, {waitUntil: "domcontentloaded"})
     const copyBtn = await page.$('.action_buttons a.copy')
     await copyBtn.click()
-    await page.waitFor(1000)
+    await page.waitFor(1500)
     console.log("[INFO] Moved apply page")
-    const autoFill = await page.evaluate(() => {
+    await page.evaluate(() => {
         begginer = document.querySelector('.custom_fields.specific_level .beginner input')
         begginer.checked = true
         moreThanOne = document.querySelector('#project_contract_hope_number_more_than_one')
@@ -51,7 +38,18 @@ async function run(){
     } else {
         console.log("[INFO] Test completed")
     }
-    await browser.close()
+    await page.close()
 }
-run()
 
+(async () => {
+    const response = apiUtil.fetch(urlUtil.api)
+    const promises = []
+    const browser = await puppeteer.browser
+    for (const url of response.offer_url) {6
+        promises.push(recruit(browser, url))
+    }
+    for (const promise of promises) {
+        await promise;
+    }
+    browser.close();
+})()
