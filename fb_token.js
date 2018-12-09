34a37f2d8c3c4ac36608b93d255810b89d17d6a8
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2018-12-09 18:44:01 +0900

    コメント追記

--- fb_token.js
+++ fb_token.js
@@ -5,13 +5,22 @@ const HUBOT_API_URL = `${process.env.HUBOT_HOST || 'http://localhost:8080/'}serv
 
 (async () => {
   let response = {}
+  // ユーザーデータをHubotから取得
   try {
     response = await axios.get(HUBOT_API_URL)
   } catch(e) {
     console.error(e)
     process.exit(1)
   }
+
+  // ユーザーデータがなければ終了
   let users = response.data
+  if(!users || !Object.keys(users).length) {
+    console.log('users not found')
+    return
+  }
+
+  // 全員分アクセストークンを取得
   for(uid in users) {
     let email = users[uid].email
     let password = Crypt.decrypt(users[uid].password)
@@ -19,14 +28,18 @@ const HUBOT_API_URL = `${process.env.HUBOT_HOST || 'http://localhost:8080/'}serv
     try {
       users[uid].fb_token = await tinderauth.getAccessToken()
       users[uid].fb_uid = await tinderauth.getUserId()
+      console.log(`User ${uid}: get new access token`)
     } catch(e) {
+      console.log(`User ${uid}: failure new access token`)
       continue
     }
   }
 
+  // Hubotに投げてRedisのデータを更新
   try {
     await axios.post(HUBOT_API_URL, users)
-    console.log('success update fb access token')
+    console.log('success update fb access token of redis')
+    return
   } catch(e) {
     console.log(e.message)
     process.exit(1)
